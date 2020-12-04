db0fac82719e695568a9cda0dacf4fd48a2a1de3
Erland366 <68678137+Erland366@users.noreply.github.com>
2020-12-04 22:49:25 +0800

    Update Twitty.java

--- Twitty.java
+++ Twitty.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 class Twitty {
   AdjacencyMatriksGraph amGraph;
 
@@ -5,16 +7,6 @@ class Twitty {
     amGraph = new AdjacencyMatriksGraph();
   }
 
-  /*
-   * TODO: INSERT USER [V] TODO: CONNECTIONG USER [] TODO: MOST FOLLOWED [] TODO:
-   * MINIMUM RETWEET [] TODO: GROUPING [] TODO: TOPIC DETECTION []
-   * 
-   */
-
-  /*
-   * Penggunaan method insertUser melibatkan method addVertex pada graph
-   * 
-   */
   public void insertUser(User usr) {
     amGraph.addVertex(usr);
     System.out.println(usr.getName() + "_inserted");
@@ -24,6 +16,12 @@ class Twitty {
     int indexUser = amGraph.getVertexIndex(username);
     int indexUser2 = amGraph.getVertexIndex(username2);
 
+    User user1 = amGraph.getUserVertex(username);
+    User user2 = amGraph.getUserVertex(username2);
+
+    user1.addFollowing(user2);
+    user2.addFollowers(user1);
+    
     amGraph.addEdge(indexUser, indexUser2);
     System.out.println("connect_" + username + "_" + username2 + "_success");
   }
@@ -43,9 +41,21 @@ class Twitty {
   public void mostFollowed() {
     Vertex[] userList = getAllUser();
     int totalUser = getTotalUser();
+    String followersTerbanyak[] = new String[totalUser];
+    int max=0;
     for (int i = 0; i < totalUser; i++) {
-
+      if(userList[i].user.getTotalFollowers()>max){
+        max = userList[i].user.getTotalFollowers();
+      }
     }
+    int counter = 0;
+    for (int i = 0; i < totalUser; i++) {
+      if(userList[i].user.getTotalFollowers()==max){
+        followersTerbanyak[counter] = userList[i].user.getName();
+        counter++;
+      }
+    }
+    System.out.println(Arrays.toString(followersTerbanyak));
   }
 
   public void minimumRetweet() {
